2eba25312b26bb96387968c668c88f619f7a05f0
DeAngelo Jones <deangelojones0708@gmail.com>
2024-02-09 10:25:31 -0600

    small modification

--- client/src/js/header.js
+++ client/src/js/header.js
@@ -5,7 +5,7 @@ const header = `
  __  / / /| | / / / __/   
 / /_/ / ___ |/ / / /___   
  ____/_/  |_/_/ /_____/   
-just another text editor
+Just another text editor
 */                          
 `;
 
